e51464988391ab0703026414d978806068424c87
Bolotov Alexandr <projectfebr@gmail.com>
2020-12-07 13:16:25 +0300

    Initial Commit

---
+++ HWS100.Project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HWS100.Project1
+//
+//  Created by Александр Болотов on 07.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
